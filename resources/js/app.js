283af4de36259cb83322f6224d4589ba297d2f15
joel morelos <joelmorelos@MacBook-Pro-de-joel.local>
2019-12-08 22:23:19 -0600

    primer commit

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import router from './router'
+require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+    router
+});
---
+++ resources/js/router.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import VueRouters from 'vue-router'
+
+
